814f297b7c565da7d089bde18c589e7fe0643a79
gabrielle <gabitorres118@gmail.com>
2020-05-28 19:18:10 -0300

    Alteração no Layout

--- AmorDoce/Models/Produtos.cs
+++ AmorDoce/Models/Produtos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,17 +8,17 @@ namespace AmorDoce.Models
 {
     public class Produtos
     {
-
+        [DisplayName("Produto")]
         public int id_produto { get; set; }
-
+        [DisplayName("Nome do Bolo")]
         public string nome_produto { get; set; }
-
+        [DisplayName("Descrição")]
         public string descricao_produto { get; set; }
-
+        [DisplayName("Preço")]
         public double preco_produto { get; set; }
-
+        [DisplayName("Validade")]
         public DateTime validade_produto { get; set; }
-
+        [DisplayName("Selecione uma imagem")]
         public string foto_caminho { get; set; }
 
     }
